7a4723306d58b75572689a8ee16d373fc09ac4f2
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-05 20:58:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,24 @@
 variable "instance_type" {
+  type    = string
   default = "t2.micro"
 }
 
-variable "ssh_public_key" {
-  description = "SSH public key for EC2 instances"
-  type        = string
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFtYQcWryY8vYWfHQHLhirjquhMRQ9Xf1ONf8+0ydL4p ubuntu@ip-172-31-11-64"
+variable "root_pass" {
+  type    = string
+  default = "ansible"
 }
 
+variable "instance_name" {
+  type    = string
+  default = "server"
+}
+
+
 variable "ssh_private_key" {
-  description = "Path to the SSH private key file"
-  type        = string
-  default     = "/home/ubuntu/.ssh/id_ed25519"
+  type    = string
+  default = "/home/ubuntu/.ssh/id_ed25519"
+}
+variable "ssh_public_key" {
+  type    = string
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFtYQcWryY8vYWfHQHLhirjquhMRQ9Xf1ONf8+0ydL4p ubuntu@ip-172-31-11-64"
 }
