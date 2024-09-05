df4571291fb6624584f99396fbeeadf9490e3e08
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-05 19:20:14 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -7,32 +7,34 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
 }
 
+# Key Pair
 resource "aws_key_pair" "example" {
   key_name   = "key02"
-  public_key = file("/home/ubuntu/.ssh/id_ed25519.pub")
+  public_key = file("~/.ssh/id_ed25519.pub")
 }
 
-resource "aws_instance" "server" {
+# Application Server
+resource "aws_instance" "app_server" {
   ami           = "ami-0522ab6e1ddcc7055"
   instance_type = var.instance_type
-  key_name      = "key02"
+  key_name      = aws_key_pair.example.key_name
 
   tags = {
-    Name = "${terraform.workspace}_server"
+    Name = "AppServer"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "cat /etc/os-release",
-      "mkdir -p /home/ubuntu/.ssh",
-      "echo '${var.ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys",
-      "chmod 600 /home/ubuntu/.ssh/authorized_keys",
-      "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
+      "sudo apt update -y",
+      "sudo apt install -y prometheus node_exporter",
+      "sudo systemctl start node_exporter",
+      "sudo systemctl enable node_exporter",
+      "sudo ufw allow 9100/tcp",
+      "sudo ufw allow 9090/tcp"
     ]
   }
 
@@ -42,12 +44,74 @@ resource "aws_instance" "server" {
     user        = "ubuntu"
     private_key = file(var.ssh_private_key)
   }
+}
 
-  provisioner "local-exec" {
-    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=/full/path/to/.ssh/id_ed25519' > inventory.ini"
+# Test Server
+resource "aws_instance" "test_server" {
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.example.key_name
+
+  tags = {
+    Name = "TestServer"
   }
 
-  provisioner "local-exec" {
-    command = "ansible-playbook -u ubuntu -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y prometheus node_exporter",
+      "sudo systemctl start node_exporter",
+      "sudo systemctl enable node_exporter",
+      "sudo ufw allow 9100/tcp",
+      "sudo ufw allow 9090/tcp"
+    ]
   }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(var.ssh_private_key)
+  }
+}
+
+# Grafana Server
+resource "aws_instance" "grafana_server" {
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.example.key_name
+
+  tags = {
+    Name = "GrafanaServer"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y grafana",
+      "sudo systemctl start grafana-server",
+      "sudo systemctl enable grafana-server",
+      "sudo ufw allow 3000/tcp"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(var.ssh_private_key)
+  }
+}
+
+# Outputs for Public IPs
+output "app_server_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+output "test_server_public_ip" {
+  value = aws_instance.test_server.public_ip
+}
+
+output "grafana_server_public_ip" {
+  value = aws_instance.grafana_server.public_ip
 }
