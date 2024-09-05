33b6395f4fb34fa85407e06fe9cbae37d6dbbf7f
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-05 20:02:18 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,8 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"  # Ensure this is a valid AWS region
+}
+
+# Key Pair
 resource "aws_key_pair" "example" {
   key_name   = "key02"
   public_key = var.ssh_public_key
 }
 
+# Application Server
 resource "aws_instance" "app_server" {
   ami           = "ami-0522ab6e1ddcc7055"
   instance_type = var.instance_type
@@ -31,6 +46,7 @@ resource "aws_instance" "app_server" {
   }
 }
 
+# Test Server
 resource "aws_instance" "test_server" {
   ami           = "ami-0522ab6e1ddcc7055"
   instance_type = var.instance_type
@@ -59,6 +75,7 @@ resource "aws_instance" "test_server" {
   }
 }
 
+# Grafana Server
 resource "aws_instance" "grafana_server" {
   ami           = "ami-0522ab6e1ddcc7055"
   instance_type = var.instance_type
@@ -86,6 +103,7 @@ resource "aws_instance" "grafana_server" {
   }
 }
 
+# Outputs for Public IPs
 output "app_server_public_ip" {
   value = aws_instance.app_server.public_ip
 }
