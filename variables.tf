88254e7c6ef22398593b479f39937961059abda4
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-06 12:13:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "instance_type" {
 variable "ssh_public_key" {
   description = "SSH public key for EC2 instances"
   type        = string
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaKktuuLiMqhMx+dlua5KGaV1X25CKjaCIVBCubacnz jenkins@ip-172-31-18-253"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJ0aLiohOZKlY2atZHQPT/vIU9wcCDjft3CR4G4sWSW jenkins@ip-172-31-29-33"
 }
 
 variable "ssh_private_key" {
