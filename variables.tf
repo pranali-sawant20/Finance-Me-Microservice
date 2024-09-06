eb84875be3be6b9c1f254274cfe1aa5ef1d97d0f
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-06 14:09:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,16 @@
 variable "instance_type" {
+  type    = string
   default = "t2.micro"
 }
 
-variable "ssh_public_key" {
-  description = "SSH public key for EC2 instances"
-  type        = string
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJ0aLiohOZKlY2atZHQPT/vIU9wcCDjft3CR4G4sWSW jenkins@ip-172-31-29-33"
+variable "root_pass" {
+  type    = string
+  default = "ansible"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "server"
 }
 
 variable "ssh_private_key" {
@@ -13,3 +18,11 @@ variable "ssh_private_key" {
   type        = string
   default     = "/var/lib/jenkins/.ssh/id_ed25519"
 }
+
+variable "ssh_public_key" {
+  description = "SSH public key for EC2 instances"
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJ0aLiohOZKlY2atZHQPT/vIU9wcCDjft3CR4G4sWSW"
+}
+
+
