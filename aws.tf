4300b933fc8b0b8c857737df9612832359b50f86
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-06 21:35:10 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
 }
@@ -15,6 +14,82 @@ provider "aws" {
 resource "aws_key_pair" "example" {
   key_name   = "key02"
   public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "aws_instance" "application_server" {
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = var.instance_type
+  key_name      = "key02"
+
+  tags = {
+    Name = "application_server"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cat /etc/os-release",
+      "mkdir -p /home/ubuntu/.ssh",
+      "echo '${var.ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys",
+      "chmod 600 /home/ubuntu/.ssh/authorized_keys",
+      "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(var.ssh_private_key)
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=~/.ssh/id_ed25519' > inventory.ini"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
+    environment = {
+      ANSIBLE_CONFIG = "ansible.cfg"
+    }
+  }
+}
+
+resource "aws_instance" "testing_server" {
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = var.instance_type
+  key_name      = "key02"
+
+  tags = {
+    Name = "testing_server"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cat /etc/os-release",
+      "mkdir -p /home/ubuntu/.ssh",
+      "echo '${var.ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys",
+      "chmod 600 /home/ubuntu/.ssh/authorized_keys",
+      "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(var.ssh_private_key)
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=~/.ssh/id_ed25519' > inventory.ini"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
+    environment = {
+      ANSIBLE_CONFIG = "ansible.cfg"
+    }
+  }
 }
 
 resource "aws_instance" "prometheus_server" {
@@ -26,13 +101,6 @@ resource "aws_instance" "prometheus_server" {
     Name = "prometheus_server"
   }
 
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file(var.ssh_private_key)
-  }
-
   provisioner "remote-exec" {
     inline = [
       "cat /etc/os-release",
@@ -41,6 +109,13 @@ resource "aws_instance" "prometheus_server" {
       "chmod 600 /home/ubuntu/.ssh/authorized_keys",
       "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
     ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(var.ssh_private_key)
   }
 
   provisioner "local-exec" {
@@ -53,92 +128,4 @@ resource "aws_instance" "prometheus_server" {
       ANSIBLE_CONFIG = "ansible.cfg"
     }
   }
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-0522ab6e1ddcc7055"
-  instance_type = var.instance_type
-  key_name      = "key02"
-
-  tags = {
-    Name = "app_server"
-  }
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file(var.ssh_private_key)
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "cat /etc/os-release",
-      "mkdir -p /home/ubuntu/.ssh",
-      "echo '${var.ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys",
-      "chmod 600 /home/ubuntu/.ssh/authorized_keys",
-      "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
-    ]
-  }
-
-  provisioner "local-exec" {
-    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=~/.ssh/id_ed25519' > inventory.ini"
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
-    environment = {
-      ANSIBLE_CONFIG = "ansible.cfg"
-    }
-  }
-}
-
-resource "aws_instance" "test_server" {
-  ami           = "ami-0522ab6e1ddcc7055"
-  instance_type = var.instance_type
-  key_name      = "key02"
-
-  tags = {
-    Name = "test_server"
-  }
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file(var.ssh_private_key)
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "cat /etc/os-release",
-      "mkdir -p /home/ubuntu/.ssh",
-      "echo '${var.ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys",
-      "chmod 600 /home/ubuntu/.ssh/authorized_keys",
-      "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
-    ]
-  }
-
-  provisioner "local-exec" {
-    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=~/.ssh/id_ed25519' > inventory.ini"
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
-    environment = {
-      ANSIBLE_CONFIG = "ansible.cfg"
-    }
-  }
-}
-
-output "prometheus_server_ip" {
-  value = aws_instance.prometheus_server.public_ip
-}
-
-output "app_server_public_ip" {
-  value = aws_instance.app_server.public_ip
-}
-
-output "test_server_public_ip" {
-  value = aws_instance.test_server.public_ip
 }
