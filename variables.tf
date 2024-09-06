cc5807312008e346d884abb11c70d4dd12e54938
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-07 01:30:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,35 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region for the EC2 instance"
+  default     = "ap-south-1"
+}
+
 variable "instance_type" {
-  type    = string
-  default = "t2.micro"
+  type        = string
+  description = "Instance type for the EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Key pair name for the EC2 instance"
+  default     = "key02"
 }
 
 variable "ssh_private_key" {
-  type    = string
-  default = "/var/lib/jenkins/.ssh/id_ed25519"
+  type        = string
+  description = "Path to the SSH private key for EC2 instance access"
+  default     = "~/.ssh/id_ed25519"
 }
 
 variable "ssh_public_key" {
-  type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5J5aJ3iY0+/qP4lLgnT0Lkcv0jEAtm+oYDMpLRAm3J"
+  type        = string
+  description = "Public SSH key for EC2 instance access"
+  default     = "~/.ssh/id_ed25519.pub"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID for launching the EC2 instance"
+  default     = "ami-0522ab6e1ddcc7055"
 }
