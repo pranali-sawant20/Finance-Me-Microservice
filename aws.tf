5867edb354b23e9a17270709940055c8d0305f57
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-06 19:51:42 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
 }
@@ -17,40 +16,56 @@ resource "aws_key_pair" "example" {
   public_key = file("~/.ssh/id_ed25519.pub")
 }
 
-resource "aws_instance" "server" {
+resource "aws_instance" "prometheus_server" {
   ami           = "ami-0522ab6e1ddcc7055"
   instance_type = var.instance_type
   key_name      = "key02"
 
   tags = {
-    Name = "${terraform.workspace}_server"
+    Name = "${terraform.workspace}_prometheus_server"
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "cat /etc/os-release",
-      "mkdir -p /home/ubuntu/.ssh",
-      "echo '${var.ssh_public_key}' >> /home/ubuntu/.ssh/authorized_keys",
-      "chmod 600 /home/ubuntu/.ssh/authorized_keys",
-      "chown -R ubuntu:ubuntu /home/ubuntu/.ssh"
-    ]
+  
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip} prometheus_server_ip' >> inventory.ini"
   }
+}
 
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file(var.ssh_private_key)
+output "prometheus_server_ip" {
+  value = aws_instance.prometheus_server.public_ip
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = var.instance_type
+  key_name      = "key02"
+
+  tags = {
+    Name = "${terraform.workspace}_app_server"
   }
 
   provisioner "local-exec" {
-    command = "echo '${self.public_ip} ansible_user=ubuntu ansible_private_key_file=~/.ssh/id_ed25519' > inventory.ini"
+    command = "echo '${self.public_ip} app_server_public_ip' >> inventory.ini"
+  }
+}
+
+output "app_server_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+resource "aws_instance" "test_server" {
+  ami           = "ami-0522ab6e1ddcc7055"
+  instance_type = var.instance_type
+  key_name      = "key02"
+
+  tags = {
+    Name = "${terraform.workspace}_test_server"
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml"
-    environment = {
-      ANSIBLE_CONFIG = "ansible.cfg"
-    }
+    command = "echo '${self.public_ip} test_server_public_ip' >> inventory.ini"
   }
+}
+
+output "test_server_public_ip" {
+  value = aws_instance.test_server.public_ip
 }
