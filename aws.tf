eac02e19de1b8350276c77c101fd072be1378b0b
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-07 13:48:23 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "server" {
     Project     = "FinanceMe"
   }
 
-  # Provisioner for remote execution
+  # Remote execution
   provisioner "remote-exec" {
     inline = [
       "echo 'Provisioning started on ${self.public_ip}'",
@@ -51,14 +51,14 @@ resource "aws_instance" "server" {
     timeout     = "5m"
   }
 
-  # Local-exec to generate inventory for Ansible
+  # Generate inventory for Ansible
   provisioner "local-exec" {
     command = <<EOF
       echo "${self.public_ip} ansible_user=ubuntu ansible_private_key_file=${var.ssh_private_key}" > inventory.ini
     EOF
   }
 
-  # Local-exec to run Ansible playbook
+  # Run Ansible playbook
   provisioner "local-exec" {
     command = <<EOF
       ansible-playbook -u ubuntu -i inventory.ini -e 'ansible_python_interpreter=/usr/bin/python3' ansible-playbook.yml
@@ -69,4 +69,40 @@ resource "aws_instance" "server" {
 output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.server.public_ip
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region for the EC2 instance"
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type for the EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Key pair name for the EC2 instance"
+  default     = "key02"
+}
+
+variable "ssh_private_key" {
+  type        = string
+  description = "Path to the SSH private key for EC2 instance access"
+  default     = "~/.ssh/id_ed25519"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Public SSH key for EC2 instance access"
+  default     = "~/.ssh/id_ed25519.pub"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID for launching the EC2 instance"
+  default     = "ami-0522ab6e1ddcc7055"
 }
