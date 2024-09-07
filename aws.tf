b1809bff8aa16fded50baf7e1ba7001ade6e73c7
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-07 13:57:53 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -70,39 +70,3 @@ output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.server.public_ip
 }
-
-variable "aws_region" {
-  type        = string
-  description = "AWS region for the EC2 instance"
-  default     = "ap-south-1"
-}
-
-variable "instance_type" {
-  type        = string
-  description = "Instance type for the EC2 instance"
-  default     = "t2.micro"
-}
-
-variable "key_name" {
-  type        = string
-  description = "Key pair name for the EC2 instance"
-  default     = "key02"
-}
-
-variable "ssh_private_key" {
-  type        = string
-  description = "Path to the SSH private key for EC2 instance access"
-  default     = "~/.ssh/id_ed25519"
-}
-
-variable "ssh_public_key" {
-  type        = string
-  description = "Public SSH key for EC2 instance access"
-  default     = "~/.ssh/id_ed25519.pub"
-}
-
-variable "ami_id" {
-  type        = string
-  description = "AMI ID for launching the EC2 instance"
-  default     = "ami-0522ab6e1ddcc7055"
-}
