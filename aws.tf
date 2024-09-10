2854769df99d960255b47b9f884a653a56df0fdc
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-10 17:33:48 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -11,13 +11,18 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Check if the key pair already exists
+data "aws_key_pair" "existing" {
+  key_name = var.key_name
+}
+
 # AWS Key Pair
 resource "aws_key_pair" "example" {
+  count      = data.aws_key_pair.existing.key_name != "" ? 0 : 1
   key_name   = var.key_name
   public_key = file(var.ssh_public_key)
-
+  
   lifecycle {
-    # Prevent recreation of key pair
     prevent_destroy = true
   }
 }
@@ -26,7 +31,7 @@ resource "aws_key_pair" "example" {
 resource "aws_instance" "server" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = aws_key_pair.example.key_name
+  key_name      = data.aws_key_pair.existing.key_name != "" ? data.aws_key_pair.existing.key_name : aws_key_pair.example.key_name
 
   tags = {
     Name        = "${terraform.workspace}_server"
