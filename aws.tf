4369c504ad3195928a0e341df76a1890ceadd0b0
Suguresh <107918971+suguslove10@users.noreply.github.com>
2024-09-10 10:05:51 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -65,7 +65,8 @@ resource "aws_instance" "server" {
   }
 }
 
-output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.server.public_ip
+# aws.tf
+
+output "public_ip" {
+  value = aws_instance.my_instance.public_ip
 }
